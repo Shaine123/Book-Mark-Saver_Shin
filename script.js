1afbc543f46e916750e20f3932e6b32e222583e6
Shaine123 <shaineberdida@gmail.com>
2026-05-22 19:39:48 +0800

    added functions for the bookmark

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const bookMarkUrl = document.getElementById("bookmark-url");
 const addBtn = document.getElementById("add-btn");
 const bookMarkList = document.getElementById("bookmark-list");
 
-// document.addEventListener("DOMContentLoaded", loadFromStorage);
+document.addEventListener("DOMContentLoaded", loadBookMarks);
 
 addBtn.addEventListener("click", addToStorage);
 
@@ -18,6 +18,55 @@ function addToStorage() {
       return;
     }
 
-    console.log("valid");
+    createBookMarkItem(name, url);
+    saveToStoStorage(name, url);
   }
+}
+
+function saveToStoStorage(name, url) {
+  const bookMarks = getBookMarksFromStorage();
+
+  bookMarks.push({
+    name,
+    url,
+  });
+
+  localStorage.setItem("bookmarks", JSON.stringify(bookMarks));
+}
+
+function getBookMarksFromStorage() {
+  const bookMarkList = JSON.parse(localStorage.getItem("bookmarks"));
+
+  return bookMarkList ? bookMarkList : [];
+}
+
+function createBookMarkItem(name, url) {
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.classList.add("bookmark-item");
+
+  const link = document.createElement("a");
+
+  link.href = url;
+  link.textContent = name;
+
+  const btn = document.createElement("button");
+
+  btn.textContent = "Remove";
+  btn.classList.add("remove-btn");
+
+  div.append(link);
+  div.append(btn);
+
+  li.append(div);
+  bookMarkList.append(li);
+}
+
+function loadBookMarks() {
+  const bookMarks = getBookMarksFromStorage();
+
+  bookMarks.forEach((bookmark) => {
+    createBookMarkItem(bookmark.name, bookmark.url);
+  });
 }
